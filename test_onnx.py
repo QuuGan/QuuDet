ba5e5053d60a444ca6473e007dfcc09a8f4729c0
mouqianxi <48301905+mouqianxi@users.noreply.github.com>
2023-11-11 16:32:32 +0800

    Update test_onnx.py

--- test_onnx.py
+++ test_onnx.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test_onnx.py')
     parser.add_argument('--weights', type=str, default='', help='model.onnx path(s)')
     parser.add_argument('--data', type=str, default='', help='*.data path')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.005, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--task', default='val', help='train, val, test, speed or study')
