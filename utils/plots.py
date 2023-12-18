0befc96f65e8ab037afc648ab3c1dd8203350157
QuuGan <143372858+QuuGan@users.noreply.github.com>
2023-12-18 16:30:14 +0800

    Add files via upload

--- utils/plots.py
+++ utils/plots.py
@@ -399,7 +399,7 @@ def plot_results_overlay(start=0, stop=0):  # from utils.plots import *; plot_re
 
 def plot_results(start=0, stop=0, bucket='', id=(), labels=(), save_dir=''):
     # Plot training 'results*.txt'. from utils.plots import *; plot_results(save_dir='runs/train/exp')
-    fig, ax = plt.subplots(2, 5, figsize=(12, 6), tight_layout=True)
+    fig, ax = plt.subplots(1, 5, figsize=(12, 6), tight_layout=True)
     ax = ax.ravel()
     s = ['Box', 'Objectness', 'Classification', 'Precision', 'Recall',
          'val Box', 'val Objectness', 'val Classification', 'mAP@0.5', 'mAP@0.5:0.95']
@@ -412,22 +412,19 @@ def plot_results(start=0, stop=0, bucket='', id=(), labels=(), save_dir=''):
         files = list(Path(save_dir).glob('results*.txt'))
     assert len(files), 'No results.txt files found in %s, nothing to plot.' % os.path.abspath(save_dir)
     for fi, f in enumerate(files):
-        try:
-            results = np.loadtxt(f, usecols=[2, 3, 4, 8, 9, 12, 13, 14, 10, 11], ndmin=2).T
-            n = results.shape[1]  # number of rows
-            x = range(start, min(stop, n) if stop else n)
-            for i in range(10):
-                y = results[i, x]
-                if i in [0, 1, 2, 5, 6, 7]:
-                    y[y == 0] = np.nan  # don't show zero loss values
-                    # y /= y[0]  # normalize
-                label = labels[fi] if len(labels) else f.stem
-                ax[i].plot(x, y, marker='.', label=label, linewidth=2, markersize=8)
-                ax[i].set_title(s[i])
-                # if i in [5, 6, 7]:  # share train and val loss y axes
-                #     ax[i].get_shared_y_axes().join(ax[i], ax[i - 5])
-        except Exception as e:
-            print('Warning: Plotting error for %s; %s' % (f, e))
+        results = np.loadtxt(f, usecols=[2, 3, 4, 8, 9, 10, 11], ndmin=2).T
+        n = results.shape[1]  # number of rows
+        x = range(start, min(stop, n) if stop else n)
+        for i in range(5):
+            y = results[i, x]
+            if i in [0, 1, 2, 5, 6, 7]:
+                y[y == 0] = np.nan  # don't show zero loss values
+                # y /= y[0]  # normalize
+            label = labels[fi] if len(labels) else f.stem
+            ax[i].plot(x, y, marker='.', label=label, linewidth=2, markersize=8)
+            ax[i].set_title(s[i])
+            # if i in [5, 6, 7]:  # share train and val loss y axes
+            #     ax[i].get_shared_y_axes().join(ax[i], ax[i - 5])
 
     ax[1].legend()
     fig.savefig(Path(save_dir) / 'results.png', dpi=200)
